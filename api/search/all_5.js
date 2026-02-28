4f702076728cc85d73c8dfc16a2a54082c8b75b5
Jena Morgan <audreysophia@gmail.com>
2026-02-28 18:20:27 -0500

    Deployed 2a7898f with MkDocs version: 1.6.1

---
+++ api/search/all_5.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['process_5fgenerator_0',['process_generator',['../battery_v2_8py.html#ab0f783128b1bd9d942d65f8da46a1da8',1,'batteryV2']]],
+  ['process_5fgenerator_5fextreme_1',['process_generator_extreme',['../battery_v2_8py.html#a5fad548f1d812962fab2729d76b85e38',1,'batteryV2']]]
+];
