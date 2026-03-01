19af2393002f5ee025c08dfeb8f12a2a3d9e9304
Jena Morgan <audreysophia@gmail.com>
2026-02-28 22:39:24 -0500

    Deployed 2a7898f with MkDocs version: 1.6.1

--- api/annotated_dup.js
+++
@@ -1,8 +0,0 @@
-var annotated_dup =
-[
-    [ "batteryV2", null, [
-      [ "BatteryConfig", "classbattery_v2_1_1_battery_config.html", "classbattery_v2_1_1_battery_config" ],
-      [ "MissionConfig", "classbattery_v2_1_1_mission_config.html", null ],
-      [ "MonteCarloGUI", "classbattery_v2_1_1_monte_carlo_g_u_i.html", "classbattery_v2_1_1_monte_carlo_g_u_i" ]
-    ] ]
-];
--- api/search/functions_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['main_0',['main',['../battery_v2_8py.html#a715155bd28d0a42bff647cd9b38fb7c9',1,'batteryV2']]]
-];
