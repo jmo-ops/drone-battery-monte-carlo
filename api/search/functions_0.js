38ceac7772b05605c369c4ea52128e05f43712b9
Jena Morgan <audreysophia@gmail.com>
2026-02-28 22:27:26 -0500

    Deployed 2a7898f with MkDocs version: 1.6.1

--- api/search/functions_0.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['_5f_5finit_5f_5f_0',['__init__',['../classbattery_v2_1_1_battery_config.html#a01bd6c0552bae56729b044ed2ce12861',1,'batteryV2::BatteryConfig']]],
-  ['_5fbuild_5ftabs_1',['_build_tabs',['../classbattery_v2_1_1_monte_carlo_g_u_i.html#a8945fb5c765a8525ce45e40daabf2065',1,'batteryV2::MonteCarloGUI']]],
-  ['_5fbuild_5fwidgets_2',['_build_widgets',['../classbattery_v2_1_1_monte_carlo_g_u_i.html#a779f1457bb1fa50d8f55c636b48c0967',1,'batteryV2::MonteCarloGUI']]],
-  ['_5fgenerate_5fparameters_5fsummary_3',['_generate_parameters_summary',['../classbattery_v2_1_1_monte_carlo_g_u_i.html#a9ac8fb9e7c9b79aac8a389fb85026221',1,'batteryV2::MonteCarloGUI']]],
-  ['_5fplot_5fdistributions_4',['_plot_distributions',['../classbattery_v2_1_1_monte_carlo_g_u_i.html#a67f25aa73b33a439ae78ad47aebbddc6',1,'batteryV2::MonteCarloGUI']]],
-  ['_5fplot_5fextreme_5fdistributions_5',['_plot_extreme_distributions',['../classbattery_v2_1_1_monte_carlo_g_u_i.html#a51e7e587954e3cceaea5ff17ad8aa75d',1,'batteryV2::MonteCarloGUI']]]
-];
--- api/search/functions_6.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['sample_5fextreme_5finputs_0',['sample_extreme_inputs',['../battery_v2_8py.html#a12a82ab0f3070f3f3081ff6589fd929b',1,'batteryV2']]],
-  ['sample_5frandom_5finputs_1',['sample_random_inputs',['../battery_v2_8py.html#af248bd08982771567965a793c77a9ab3',1,'batteryV2']]],
-  ['simulate_5fmission_2',['simulate_mission',['../battery_v2_8py.html#aecfb42e205fe14aa239afe1e5f5981cb',1,'batteryV2']]],
-  ['summarize_5fresults_3',['summarize_results',['../battery_v2_8py.html#a0af7214a83432d16096b4812474093be',1,'batteryV2']]]
-];
